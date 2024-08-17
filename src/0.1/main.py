94350e67f9376e20d36fb95a64c626e9bab8698c
AD-SEC-dev-admin <adulisseca@gmail.com>
2024-08-17 18:05:43 +0300

    Update main.py

--- src/0.1/main.py
+++ src/0.1/main.py
@@ -3,15 +3,7 @@
 """
 	* Copyright(c) 2024, ADULIS(R) SECURITIES. All rights reserved.
     * Copyright(c) JEGOL(TM) Decentralized Exchange.
-    
-    * Proof-Of-Concept
-		* Initial: Telegram-based, Blockchain DB iteration (Blockchain with RDB).
-			* Creates a Block/s w/ (and writes it into the DB). No data abstraction (i.e. the Blocks do not carry user-requested data, like transactions, etc.)
-				* Id.col (primary key) -> Irrelevant to the Block abstraction.
-				* timestamp.col
-				* _Username.col
-				* Username.col
-				* Hash.col
-				* prevHash.col
 """
+
+__ver__: int = 0.1
 
